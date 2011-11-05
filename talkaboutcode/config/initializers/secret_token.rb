cd8ac34e9258a15bb0888c2d6b01349ab0ad95b0
Thor Kell <djfractal@gmail.com>
2011-11-05 15:01:14 -0400

    Removing old version.

--- talkaboutcode/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Talkaboutcode::Application.config.secret_token = '96e9f256a8181dce3f19c5a0105bad1f6d77661894fd3a345afb71fa4114e79cbf92e0b432f479a63a3b5fcf9d89059671d0110301f318162ac9cffe432d6a9c'
