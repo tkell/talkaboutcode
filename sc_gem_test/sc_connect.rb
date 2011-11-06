56f29743ed0e25d0ec48f249bbe817ff7411facf
Thor Kell <djfractal@gmail.com>
2011-11-06 14:20:43 -0500

    Baby rails script for connecting to Soundcloud.

---
+++ sc_gem_test/sc_connect.rb
@@ -0,0 +1,18 @@
+# Plaintext passwords, baby.
+
+require 'rubygems'
+require 'soundcloud'
+
+client = Soundcloud.new({
+    :client_id => 'aa0146325fed3c61bc6e62357f8b3245',
+    :client_secret => '9f4ec2e6e58186291e4d103370fda2ab',
+    :username => 'talkaboutcode@hotmail.com',
+    :password => 'tobyjasonomarthor'
+    })
+
+puts client.get('/me').username
+
+
+
+
+
