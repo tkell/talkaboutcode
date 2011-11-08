798acb82a57d1f13ac40a645e567ecc9801e345b
Thor Kell <djfractal@gmail.com>
2011-11-08 01:03:55 -0500

    Real passwords.  Wooo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,51 @@
+require 'digest'
+
 class User < ActiveRecord::Base
+    attr_accessible :username, :email, :password
     has_many :posts
 
     # Uniqueness will start to fail if traffic increases.  Not worried about that at all for now.
+    # ^^ This is in 6.22 of the Rails tutorial, if I decide that I need it.
     validates :username, :presence => true, :uniqueness => true 
     validates :email, :presence => true, :uniqueness => true
     validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}
 
-  # I AM NOT STORING PASSWORDS PROPERLY.  I NEED TO FIX THIS BEFORE I DEPLOY THIS TO THE GENERAL PUBLIC
-  # NO, REALLY, I NEED TO FIX THIS BEFORE I LET OTHER PEOPLE USE THIS.  
+    before_save :encrypt_password
+    
+
   def self.authenticate(username, submitted_password)
     user = User.find_by_username(username)
     return nil  if user.nil?
-    return user if user.password == submitted_password
+    return user if user.has_password?(submitted_password)
   end
 
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
   end
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt unless has_password?(password)
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+
 
 end
