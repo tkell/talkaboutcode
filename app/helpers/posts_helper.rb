6aadeef346db66ce88586d0890af747c3f2bd531
Thor Kell <djfractal@gmail.com>
2011-11-08 22:39:22 -0800

    heroku debugging

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,13 @@
 require 'uri'
 require 'net/http'
-require 'rubygems'
-require 'json'
 
 module PostsHelper
 
     def embed(url)
         url = "http://soundcloud.com/oembed?url=" +url + "&format=json"
         r = Net::HTTP.get(URI(url))
+        puts "debugz"
+        puts r
         oembed_results = JSON.parse(r)
         oembed_results["html"].html_safe
     end
