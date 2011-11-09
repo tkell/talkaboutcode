9be7a5565619ce025733ab80e2117a13f94c2b35
Thor Kell <djfractal@gmail.com>
2011-11-08 22:35:15 -0800

    heroku debugging

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -9,8 +9,6 @@ module PostsHelper
         url = "http://soundcloud.com/oembed?url=" +url + "&format=json"
         r = Net::HTTP.get(URI(url))
         oembed_results = JSON.parse(r)
-        puts "debugz"
-        puts oembed_results["html"].html_safe.class
         oembed_results["html"].html_safe
     end
 
