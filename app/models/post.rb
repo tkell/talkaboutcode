f634dc53be703fd8ef75dd60d9864cfd4b275ee6
Thor Kell <djfractal@gmail.com>
2011-11-28 15:51:27 -0800

    Removing validation for code snippet.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,9 @@ class Post < ActiveRecord::Base
   acts_as_taggable
 
   # I don't think I want to validate audio_url, because it might take a while to appear...
+  # Turning off code_snippet.  Ooops    
   validates :title, :presence => true
-  validates :code_snippet, :presence => true
+  # validates :code_snippet, :presence => true
 
   default_scope :order => 'posts.created_at DESC'
 end
